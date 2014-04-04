15a85883649991c941f8ae2a986e77d4427b03eb
s518822 <s518822@NW26893.nwmissouri.edu>
2014-04-03 19:32:15 -0500

    Yaparthy

---
+++ src/yaparthigit/YaparthiGit.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package yaparthigit;
+
+/**
+ *
+ * @author S518755
+ */
+public class YaparthiGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Srujan Yaparthi");
+    }
+}
